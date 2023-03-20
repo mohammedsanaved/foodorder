269da22673da29670ea30ff5dce5ee5f621400e2
Mohammed Sanaved <97124205+mosanaved@users.noreply.github.com>
2023-03-20 14:43:24 +0530

    update index.html
    comments

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import mealsImages from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImages} alt="imagemeals" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
